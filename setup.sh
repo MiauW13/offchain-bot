eefeb2450f9effd9165b6c61f3035a386d523e1f
MiauW13 <92130095+MiauW13@users.noreply.github.com>
2022-04-06 22:52:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 sudo apt-add-repository ppa:redislabs/redis
 sudo apt-get install redis-server -y
 sudo service redis-server start
-
-
 sudo apt-get update
 sudo apt-get install apt-transport-https curl gnupg -yqq
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
@@ -11,7 +9,6 @@ curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89
 sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
 sudo apt-get update
 sudo apt-get install sbt
-
 wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.12-3.1.0.tgz
 sudo tar xhf kafka_2.12-3.1.0.tgz
 sudo rm -rf kafka_2.12-3.1.0.tgz
