89d42175e5d2212f92839d0fb9ff92322b38fc92
MiauW13 <92130095+MiauW13@users.noreply.github.com>
2022-04-06 23:58:06 +0700

    Add files via upload

--- offchain-bot.sh
+++ offchain-bot.sh
@@ -1,3 +1,18 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https curl gnupg -yqq
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sud>
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee />
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2E>
+sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+sudo apt-get update
+sudo apt-get install sbt
+
+ sudo apt-add-repository ppa:redislabs/redis
+ sudo apt-get update
+ sudo apt-get upgrade
+ sudo apt-get install redis-server
+ sudo service redis-server restart
+
 mkdir Amm-executor
 mkdir Pool-resolver
 mkdir Utxo-tracker
@@ -6,8 +21,7 @@ cd ergo-dex-backend
 sbt amm-executor/assembly
 sbt pool-resolver/assembly
 sbt utxo-tracker/assembly
-cd ~/offchain-bot
-
+cd ~/test
 sudo cp `find . -name AmmExecutor-*.jar` Amm-executor/Amm-executor.jar
 sudo cp `find . -name PoolResolver-*.jar` Pool-resolver/Pool-resolver.jar
 sudo cp `find . -name UtxoTracker-*.jar` Utxo-tracker/Utxo-tracker.jar
@@ -21,9 +35,5 @@ sudo cp utxo.conf Utxo-tracker/application.conf
 sudo rm -rf amm.conf
 sudo rm -rf pool.conf
 sudo rm -rf utxo.conf
-
-sudo curl https://dlcdn.apache.org/kafka/3.1.0/kafka_2.12-3.1.0.tgz -o kafka.tgz
-sudo tar xhf kafka.tgz
-sudo rm -rf kafka.tgz
  
 
