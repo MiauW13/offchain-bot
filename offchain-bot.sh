5070fdecba19137939b4e3880bebd2c9ca72fbee
MiauW13 <92130095+MiauW13@users.noreply.github.com>
2022-08-17 15:51:28 +0700

    Update offchain-bot.sh

--- offchain-bot.sh
+++ offchain-bot.sh
@@ -1,17 +1,17 @@
-sudo apt-get update
-sudo apt-get install apt-transport-https curl gnupg -yqq
-echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
-echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
-sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
-sudo apt-get update
-sudo apt-get install sbt
+apt-get update && apt-get upgrade
+apt-get install apt-transport-https curl gnupg -yqq
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+apt-get update
+apt-get install sbt
 
- sudo apt-add-repository ppa:redislabs/redis
- sudo apt-get update
- sudo apt-get upgrade
- sudo apt-get install redis-server
- sudo service redis-server restart
+ apt-add-repository ppa:redislabs/redis
+ apt-get update
+ apt-get upgrade
+ apt-get install redis-server
+ service redis-server restart
 
 mkdir Amm-executor
 mkdir Pool-resolver
@@ -24,23 +24,23 @@ sbt utxo-tracker/assembly
 
 cd ~/offchain-bot
 
-sudo cp `find . -name AmmExecutor-*.jar` Amm-executor/Amm-executor.jar
-sudo cp `find . -name PoolResolver-*.jar` Pool-resolver/Pool-resolver.jar
-sudo cp `find . -name UtxoTracker-*.jar` Utxo-tracker/Utxo-tracker.jar
+cp `find . -name AmmExecutor-*.jar` Amm-executor/Amm-executor.jar
+cp `find . -name PoolResolver-*.jar` Pool-resolver/Pool-resolver.jar
+cp `find . -name UtxoTracker-*.jar` Utxo-tracker/Utxo-tracker.jar
 
-sudo rm -rf ergo-dex-backend
+rm -rf ergo-dex-backend
 
-sudo cp amm.conf Amm-executor/application.conf
-sudo cp pool.conf Pool-resolver/application.conf
-sudo cp utxo.conf Utxo-tracker/application.conf
+cp amm.conf Amm-executor/application.conf
+cp pool.conf Pool-resolver/application.conf
+cp utxo.conf Utxo-tracker/application.conf
 
-sudo rm -rf amm.conf
-sudo rm -rf pool.conf
-sudo rm -rf utxo.conf
+rm -rf amm.conf
+rm -rf pool.conf
+rm -rf utxo.conf
 
 cd ~
  
 curl https://dlcdn.apache.org/kafka/3.1.0/kafka_2.12-3.1.0.tgz -o kafka.tgz
 tar xvf kafka.tgz
-sudo rm -rf kafka.tgz
+rm -rf kafka.tgz
  
